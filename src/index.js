fcb67ef4e9c7518e9e9d805dd3bc9ae25a6eb2cd
Jian <jyang173173@gmail.com>
2025-08-31 17:08:17 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import StarRating from './StarRating';
+
+// function Test() {
+// 	const [rating, setRating] = useState(0);
+
+// 	return (
+// 		<div>
+// 			<StarRating color="blue" maxRating={10} onSetRating={setRating} />
+// 			<h3>This movie was {rating} stars.</h3>
+// 		</div>
+// 	);
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+		{/* <StarRating maxRating={10} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+		<StarRating size={24} color="red" className="test" defaultRating={3} />
+		<Test /> */}
+	</React.StrictMode>
+);
